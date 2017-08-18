7146d1d69df42fdcda20d6272f8a08728c39c042
Daiane <daiane.macedo@hotmail.com>
2017-08-18 16:36:07 -0300

    corrigindo diretorios

--- src/basicweb/FireFoxDriverDemo.java
+++ src/basicweb/FireFoxDriverDemo.java
@@ -1,20 +1,22 @@
 package basicweb;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FireFoxDriverDemo {
 
-	public static void main(String[] args) {
-		
-		
+	@Test
+	public void teste() {
+			
 		// set property just for the 3.x webdriver
 		// I need the gecko driver to it works
-		System.setProperty("webdriver.gecko.driver", "/home/adtsys/selenium_drivers/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "/home/daiane.macedo/Desktop/Daiane/seleniumDrivers/geckodriver");
 		WebDriver driver = new FirefoxDriver();
-		String baseURL = "http://www.google.com";
+		String baseURL = "http://www.uol.com.br";
 		driver.get(baseURL);
+	
 	
 		
 
